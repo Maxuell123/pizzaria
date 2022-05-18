e823016cf05ab9e0ecec773f0b90fc961e5a09e3
Maxuell Lima <71743257+Maxuell123@users.noreply.github.com>
2022-05-18 19:34:35 -0300

    :package: nova funcionalidade

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
+// NAVBAR
 let navbar = document.querySelector('.header .flex .navbar');
 
 document.querySelector('#menu-btn').onclick = () => {
     navbar.classList.toggle('active');
 }
 
+// USER ACCOUNT
+let account = document.querySelector('.user-account');
+
+document.querySelector('#user-btn').onclick = () => {
+    account.classList.add('active');
+}
+
+document.querySelector('#close-account').onclick = () => {
+    account.classList.remove('active');
+}
+// USER ACCOUNT
+
 window.onscroll = () => {
     navbar.classList.remove('active');
 };
+
+
