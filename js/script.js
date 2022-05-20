b2177b12d2c09ad4db871b61f99cd39845cbcfd2
Maxuell Lima <71743257+Maxuell123@users.noreply.github.com>
2022-05-20 20:21:35 -0300

    :up: atualização

--- js/script.js
+++ js/script.js
@@ -17,8 +17,35 @@ document.querySelector('#close-account').onclick = () => {
 }
 // USER ACCOUNT
 
+
+// MY ORDERS
+let myOrders = document.querySelector('.my-orders');
+
+document.querySelector('#order-btn').onclick = () =>{
+   myOrders.classList.add('active');
+}
+
+document.querySelector('#close-orders').onclick = () =>{
+   myOrders.classList.remove('active');
+}
+// MY ORDERS
+
+// SHOPPING-CART
+let cart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+   cart.classList.add('active');
+}
+
+document.querySelector('#close-cart').onclick = () =>{
+   cart.classList.remove('active');
+}
+
+
 window.onscroll = () => {
     navbar.classList.remove('active');
+    myOrders.classList.remove('active');
+    cart.classList.remove('active');   
 };
 
 
