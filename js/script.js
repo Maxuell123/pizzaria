bf6a2d07012290a7c2e77f8822617aebd129e476
Maxuell Lima <71743257+Maxuell123@users.noreply.github.com>
2022-05-26 19:38:39 -0300

    :up: atualização

--- js/script.js
+++ js/script.js
@@ -49,3 +49,20 @@ window.onscroll = () => {
 };
 
 
+//HOME SLIDE
+let slides = document.querySelectorAll('.home-bg .home .slide-container .slide');
+let index = 0;
+
+function next() {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev() {
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+
